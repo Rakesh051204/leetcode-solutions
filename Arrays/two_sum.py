972dbe5b65be767f43e45cf693e5d19eb04e2adb
Rakesh  Palani <144886621+Rakesh051204@users.noreply.github.com>
2026-05-22 23:19:55 +0530

    Update two_sum.py

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -7,3 +7,39 @@ class Solution:
             if diff in hashmap:
                 return [hashmap[diff], i]
             hashmap[num] = i
+SQL/employees_highest_salary.sql
+SELECT e1.name AS Employee
+FROM Employee e1
+JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
+README.md
+# LeetCode Solutions 🚀
+
+This repository contains my LeetCode problem solutions.
+
+## Languages
+- Python
+- SQL
+
+## Topics
+- Arrays
+- Strings
+- SQL Queries
+
+## Profile
+- LeetCode: https://leetcode.com/
+- GitHub: https://github.com/Rakesh051204
+leetcode-solutions/
+│
+├── Arrays/
+│   ├── two_sum.py
+│   ├── best_time_to_buy_sell_stock.py
+│
+├── Strings/
+│   ├── longest_palindromic_substring.py
+│
+├── SQL/
+│   ├── employees_highest_salary.sql
+│
+├── README.md
